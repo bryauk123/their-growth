d9c1ab8ead812102057abdd96db704103365de8b
bryauk123 <91648730+bryauk123@users.noreply.github.com>
2023-01-25 09:08:19 -0800

    Initial Commit

---
+++ their-growth/their_growthApp.swift
@@ -0,0 +1,17 @@
+//
+//  their_growthApp.swift
+//  their-growth
+//
+//  Created by Bryan Apodaca on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct their_growthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
