996a5d465da4b8473bd6f467a10c3ea6d79d61f2
bryauk123 <91648730+bryauk123@users.noreply.github.com>
2023-02-15 13:24:51 -0800

    added files to work on for motorskills

---
+++ their-growth/Model/MotorMilestone.swift
@@ -0,0 +1,12 @@
+//
+//  MotorMilestone.swift
+//  their-growth
+//
+//  Created by Bryan Apodaca on 2/15/23.
+//
+
+import Foundation
+
+struct MotorMilestone{
+    var title: String
+}
---
+++ their-growth/Views/MotorskillsListView.swift
@@ -0,0 +1,23 @@
+//
+//  MotorskillsListView.swift
+//  their-growth
+//
+//  Created by Bryan Apodaca on 2/15/23.
+//
+
+import SwiftUI
+
+struct MotorskillsListView: View {
+    var body: some View {
+       Text("Motorskills List")
+        
+        
+    }
+    
+}
+
+struct MotorskillsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotorskillsListView()
+    }
+}
